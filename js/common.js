818776f1c17bfb9e4826217cc5a0423f830b9722
inagayukuma <gaqui@inagayukumanoMacBook-Pro.local>
2021-02-24 21:27:39 +0900

    変更した内容をメモ

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$('.slick01').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+          }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+               }
+          ]
+    });
+
+
+ 
